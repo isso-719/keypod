2f14581be3cf82a006946a65455fc6e74aeb43f6
Kazuki Isogai <kazu0719_i@ybb.ne.jp>
2020-06-08 18:45:53 +0900

    add validates

--- models.rb
+++ models.rb
@@ -7,11 +7,24 @@ end
 class User < ActiveRecord::Base
   has_secure_password
   has_many :workspaces
+
+  validates :name,
+    presense: true,
+    format: { with: /^[0-9a-zA-Z]*$/ },
+    length: { maximum: 7 }
+  validates :mail,
+    presense: true,
+    format: { with: /^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/ }
+  validates :password_digest,
+    length: { maximum: 7 }
 end
 
 class Workspace < ActiveRecord::Base
   has_many :keys
   belongs_to :user
+
+  validates :url,
+    presense: true
 end
 
 class Key < ActiveRecord::Base
